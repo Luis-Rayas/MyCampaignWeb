5a29f7a05b376530b59506b87f91250afe201293
Luis Reyes Rayas <luis.reyes7482@alumnos.udg.mx>
2023-03-02 16:35:38 -0600

    Se añaden los tipos de voluntarios a la migracion

--- database/migrations/2022_12_25_004444_type_volunteer_table.php
+++ database/migrations/2022_12_25_004444_type_volunteer_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\TypeVolunteer;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,11 +18,21 @@ class TypeVolunteerTable extends Migration
         Schema::create('type_volunteers', function (Blueprint $table) {
             $table->id();
 
-            $table->string('name', 15);// 0 -> Representante general, 1 -> Representante de casilla, 2 -> Otro
+            $table->string('name', 25);// 0 -> Representante general, 1 -> Representante de casilla, 2 -> Otro
 
             $table->timestamps();
             $table->softDeletes();
         });
+        //TODO Cambiar esto por la ejecucion del seeder
+        $type = new TypeVolunteer();
+        $type->name = 'Representante legal';
+        $type->save();
+        $type = new TypeVolunteer();
+        $type->name = 'Representante casilla';
+        $type->save();
+        $type = new TypeVolunteer();
+        $type->name = 'Otro';
+        $type->save();
 
         Schema::table('aux_volunteers', function (Blueprint $table) {
             $table->foreignId('type_volunteer_id')->constrained();
